33e5b85602fc6f47d3059546987dbf0fed452341
Manaz <farhath.manaz777@gmail.com>
2019-08-30 16:24:06 +0530

    Updates

---
+++ precache-manifest.58fc32e8c617c5467091d5bb1c529722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c400f81711deac9fb7cb30291a8bf46",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "6feb9665095bc1cf5e59",
+    "url": "/myapp/static/css/main.436243a0.chunk.css"
+  },
+  {
+    "revision": "d983d4cc32a1d16021a7",
+    "url": "/myapp/static/js/2.2aa0fc70.chunk.js"
+  },
+  {
+    "revision": "6feb9665095bc1cf5e59",
+    "url": "/myapp/static/js/main.8db80def.chunk.js"
+  },
+  {
+    "revision": "f7cecd4dea7363ea59da",
+    "url": "/myapp/static/js/runtime~main.f6141d4c.js"
+  }
+]);
